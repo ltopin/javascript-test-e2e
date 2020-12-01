c73c5d500828354ba1fa62ef387313e6da11a121
Topin <ltopin@gmail.com>
2020-12-01 13:45:32 -0300

    - Ajustes com o ESLINT

--- api.js
+++ api.js
@@ -1,51 +1,51 @@
-const http = require('http')
+const http = require("http");
+
 const DEFAULT_USER = {
-    username: "ltopin",
-    password: "bla123"
-}
+  username: "ltopin",
+  password: "bla123",
+};
 
 const routes = {
+  "/contact:get": (req, res) => {
+    res.write("contact us page");
+    return res.end();
+  },
+  "/login:post": async (req, res) => {
+    // response é um iterator !
 
-    '/contact:get': (req, res) => {
-        res.write('contact us page')
-        return res.end()
-    },
-    '/login:post': async (req, res) => {
-        // response é um iterator !
-
-        for await (const data of req) {
-            const user = JSON.parse(data)
-            if(user.username !== DEFAULT_USER.username || user.password !== DEFAULT_USER.password) {
-                res.writeHead(401)
-                res.write("Logging failed !")
-                return res.end()
-            }
-            res.write("Logging has succeeded !")
-            return res.end()
-        }
-    },
-    default: (req, res) => {
-
-    res.write('Hello World !')
-    return res.end()
+    for await (const data of req) {
+      const user = JSON.parse(data);
+      if (
+        user.username !== DEFAULT_USER.username ||
+        user.password !== DEFAULT_USER.password
+      ) {
+        res.writeHead(401);
+        res.write("Logging failed !");
+        return res.end();
+      }
+      res.write("Logging has succeeded !");
+      return res.end();
     }
+    return res.end;
+  },
+  default: (req, res) => {
+    res.write("Hello World !");
+    return res.end();
+  },
+};
+
+function handler(req, res) {
+  const { url, method } = req;
+  const routeKey = `${url}:${method.toLowerCase()}`;
+  const chosen = routes[routeKey] || routes.default;
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+  return chosen(req, res);
 }
 
-const handler = function (req, res) {
-    const { url, method } = req
-    const routeKey = `${url}:${method.toLowerCase()}`
-    const chosen = routes[routeKey] || routes.default
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    })
-    return chosen(req, res)
+const app = http.createServer(handler).listen(3000, () => {
+  console.log("app running on port ", 3000);
+});
 
-}
-
-const app = http.createServer(handler)
-                .listen(3000, () => {
-                    console.log('app running on port ', 3000)
-                })
-
-
-module.exports = app                
+module.exports = app;
